d48b4e4d5d0fda5d76b5613efdd8d7b73701ac3d
twimpse <158061275+twimpse@users.noreply.github.com>
2026-05-07 15:31:33 +0200

    reorder code

---
+++ src/polymorph.h
@@ -0,0 +1,25 @@
+void junk_ist();
+
+void delay_jitter_entropy();
+
+void delay_adaptive_timing();
+
+void delay_busy_loop();
+
+void delay_math_ops();
+
+void delay_syscall();
+
+void delay_cache();
+
+void delay_rdtsc();
+
+void delay_none();
+
+void delay_random_timing();
+
+int noop();
+
+int fake_noop();
+
+int noop_action();
